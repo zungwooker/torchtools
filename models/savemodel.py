3a9ae8491ad4a2fac3da609937e59186b937d7c3
zungwooker <zungwooker@gmail.com>
2024-05-06 19:52:02 +0900

    savemodel

--- models/savemodel.py
+++ models/savemodel.py
@@ -1,19 +1,19 @@
 from collections import OrderedDict
 import torch
 
-# args = OrderedDict()
-# args['model_type'] = 'MLP'
-# args['dataset'] = 'CMNISTZW'
-# args['lr'] = 0.001
-# args['epochs'] = 100
-# args['trainigtime'] = '1h23m'
-# args['projcode'] = 'TESTEST'
+# model_args = OrderedDict()
+# model_args['model_type'] = 'MLP'
+# model_args['dataset'] = 'CMNISTZW'
+# model_args['lr'] = 0.001
+# model_args['epochs'] = 100
+# model_args['trainigtime'] = '1h23m'
+# model_args['projcode'] = 'TESTEST'
 
-def savemodel(model, args: OrderedDict, save_dir='/mnt/sdc/zungwooker/workspace/debiasing/data/output/models/'):
-    model_name = f"{args['model_type']}_{args['dataset']}"
-    for key in args.keys():
+def savemodel(model, model_args: OrderedDict, save_dir='/mnt/sdc/zungwooker/workspace/debiasing/data/output/models/'):
+    model_name = f"{model_args['model_type']}_{model_args['dataset']}"
+    for key in model_args.keys():
         if key not in ['model_type', 'dataset']:
-            model_name += f"_{key}{args[key]}"
+            model_name += f"_{key}{model_args[key]}"
             
     model.eval()
     torch.save(model.state_dict(), f"{save_dir}{model_name}.pth")
